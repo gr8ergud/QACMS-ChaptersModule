17bf86d714b5a1a38f28240c7a85f60f1b04e0b0
Ravi Kandikonda <rkandikonda@gmail.com>
2013-05-14 13:50:38 -0500

    Update InstallComponent.php

--- Controller/Component/InstallComponent.php
+++ Controller/Component/InstallComponent.php
@@ -1,5 +1,37 @@
-<?php
+class InstallComponent extends Component {
+    // Return a non-true result halt the install operation.
+    public function beforeInstall() {
+        return true;
+    }
 
+    // Called after each successful install operation.
+    public function afterInstall() {
+    }
 
+    // Return a non-true result halt the uninstall operation.
+    public function beforeUninstall() {
+        return true;
+    }
 
-?>
+    // Called after each successful uninstall operation.
+    public function afterUninstall() {
+    }
+
+    // Return a non-true result halt the enable operation.
+    public function beforeEnable() {
+        return true;
+    }
+
+    // Called after each successful enable operation.
+    public function afterEnable() {
+    }
+
+    // Return a non-true result halt the disable operation.
+    public function beforeDisable() {
+        return true;
+    }
+
+    // Called after each successful disable operation.
+    public function afterDisable() {
+    }
+}
